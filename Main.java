03518770f5f53d35305d621b753e227130e6dcc3
John-3121 <johncarlocabadsan@gmail.com>
2025-12-24 15:13:03 +0800

    first commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //	System.out.println("hello");
+        Car car1 = new Car(1,"Toyota","Vios",2500.00,5);
+        Car car2 = new Car(2,"ferrai","GT",5000.00,4);
+        
+      //  Vehicle[] vehicles = {car1,car2};
+        System.out.println("Available_Rentable_Vehicles");
+      /*  for(Vehicle ve:vehicles){
+            ve.display_info();
+        }
+        */
+    }
+}
---
+++ Rentable.java
@@ -0,0 +1,6 @@
+package com.example;
+
+public interface Rentable {
+    void rent();
+    void return_vehicle();
+}
