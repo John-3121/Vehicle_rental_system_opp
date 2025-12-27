27c8dcfd614d3b1dd7a879e762f8657530d2c41d
John-3121 <johncarlocabadsan@gmail.com>
2025-12-27 10:14:14 +0800

    addnewcodes

---
+++ Customer.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Customer{
+    String name;
+    int uId;
+   public Customer(String name,int uId){
+        this.name = name;
+        this.uId = uId;
+    }
+    
+}
--- Main.java
+++ Main.java
@@ -1,16 +1,32 @@
 package com.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
         //	System.out.println("hello");
-        Car car1 = new Car(1,"Toyota","Vios",2500.00,5);
+     /*   Car car1 = new Car(1,"Toyota","Vios",2500.00,5);
         Car car2 = new Car(2,"ferrai","GT",5000.00,4);
         
-      //  Vehicle[] vehicles = {car1,car2};
+       Vehicle[] vehicles = {car1,car2};
         System.out.println("Available_Rentable_Vehicles");
-      /*  for(Vehicle ve:vehicles){
+        for(Vehicle ve:vehicles){
             ve.display_info();
+        }*/
+        boolean exit = false;
+        while(exit){
+           /* System.out.println("=== VEHICLE RENTAL SYSTEM ===\n
+               1. Show all vehicles\n
+               2. Rent a vehicle\n
+               3. Return a vehicle\n
+               4. View rentals\n
+               5. Exit\n
+               Enter choice:");
+            */
+            System.out.println("1.show all vehicles\n");
+                 int choice = sn.nextInt();
+            
         }
-        */
+        
     }
 }
